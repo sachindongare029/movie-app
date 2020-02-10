071d9eb355549c1a976d43299cee88c09e761a64
sqr <sqr@sqrs-Mac-mini.local>
2020-02-10 21:48:23 +0530

    changes

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,20 +1,19 @@
-import React, { useEffect } from "react";
+import React, {useState, useEffect } from "react";
 import axios from 'axios';
 
 function MovieList() {
+  const [cards, setCards] = useState([]);
+
   useEffect(() => {
     axios
       .get(
         "https://api.themoviedb.org/3/trending/movie/week?api_key=64e5e52b0be480556671e1b5f5e825bd"
       )
-      .then(function(response) {
-        console.log(response.data.results);
-      })
-      .catch(function(error) {
-        console.log(error);
-      })
-  });
+      .then(res => setCards(res.data.results))
+      .catch(error => console.log(error))
+  }, []);
 
+  console.log("state", cards);
   return (
     <div className="container">
       <div className="movie__container">
@@ -22,7 +21,29 @@ function MovieList() {
           <h2>Latest</h2>
         </div>
         <div className="movie__list__container">
-          hello 
+          {cards && cards.map((card) => {
+            return (
+              <div key={card.id} className="movie__wrapper">
+                <div className="movie__inner">
+                  <div className="movie__image">
+                    <img
+                      src={
+                        "https://image.tmdb.org/t/p/w500/" + card.poster_path
+                      }
+                      alt="Movie Poster"
+                    />
+                  </div>
+                  <div className="movie__name">
+                    <h4>{card.title}</h4>
+                  </div>
+                  <div className="movie__ratings">
+                    <span>{card.vote_average}</span>
+                    <a href="#">Show more</a>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
